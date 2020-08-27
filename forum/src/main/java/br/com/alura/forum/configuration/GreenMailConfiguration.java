087fd35ef26c9a8626aa682afa90f390598745a9
Alefh Sousa <alefh.sousa@gmail.com>
2020-08-27 19:28:24 -0300

    implementando profiles para ter servidor de email mockado

---
+++ forum/src/main/java/br/com/alura/forum/configuration/GreenMailConfiguration.java
@@ -0,0 +1,47 @@
+package br.com.alura.forum.configuration;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Profile("dev")
+@Configuration
+public class GreenMailConfiguration {
+
+    private GreenMail greenMail;
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.host}")
+    private String localAddress;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private Integer port;
+
+
+
+    @PostConstruct
+    public void setup() {
+        ServerSetup serverSetup = new ServerSetup(port, host, "smtp");
+        this.greenMail = new GreenMail(serverSetup);
+        this.greenMail.setUser(username, username, password);
+        this.greenMail.start();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        this.greenMail.stop();
+    }
+}
